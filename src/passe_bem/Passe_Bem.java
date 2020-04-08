ec8848d43f514e31ae0c611eae0a431027b48eb3
Lucas <Lucas@Lucas-PC>
2020-04-08 20:57:53 -0300

    Alteraçao init PASSEBEM

--- src/passe_bem/Passe_Bem.java
+++ src/passe_bem/Passe_Bem.java
@@ -4,7 +4,7 @@ public class Passe_Bem {
 
     public static void main(String[] args) {
         
-        String init = "Hello, Word!";
+        String init = "PASSEBEM";
         System.out.println(init);      
     }    
 }
