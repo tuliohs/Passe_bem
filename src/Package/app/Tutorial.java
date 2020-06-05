28ca5a452b708808f2041d1e43bb1597840aa121
Makin <makinrlz@hotmail.com>
2020-06-05 18:37:45 -0300

    JavaDoc Tutorial

--- src/Package/app/Tutorial.java
+++ src/Package/app/Tutorial.java
@@ -1,6 +1,18 @@
 package Package.app;
+/**
+* Menu recebe os métodos herdado da classe OpçõesMenu e declara os métodos para
+* autorização de acordo com a Opção escolhida pelo usuário que está logado no
+* sistema.
+* @author Thiago Soares, Tulio Henrique, Adair Lucas
+* @version 1.05
+*/
 
 public class Tutorial extends Usuario {
+
+    /**Herda Usuário, insere ID, descricao, opcoes de video, insere comentario e 
+ * opcoes de curtidas.
+     * @return 
+ */
 
     public int getId() {
         return id;
